de5430b9d9ffe964b80713ba65e1b16baf318019
Bryan Andrade <bryand1@gmail.com>
2018-06-26 09:58:05 -0700

    sys.excepthook

---
+++ excepthook.py
@@ -0,0 +1,23 @@
+import logging
+import sys
+import traceback
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format="%(levelname)s:%(name)s:%(pathname)s:%(lineno)d:%(message)s")
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logging.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+    # To format exception on one line
+    # logging.critical("Uncaught exception:%s", traceback.format_exception(
+    #     exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_exception
+
+cause['NameError']
