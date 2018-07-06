cc3bc811cf1e4df5b838e95908ed87a4a421e5c2
Bryan Andrade <bryand1@gmail.com>
2018-07-06 10:09:37 -0700

    Examine iterators

---
+++ javascript/make-iterator.js
@@ -0,0 +1,17 @@
+function makeIterator(array) {
+    let index = 0
+    return {
+        next: function() {
+            return index < arr.length ?
+                {value: arr[index++], done: false} :
+                {done: true};
+        }
+    };
+}
+
+arr = [1, 2, 3]
+it = makeIterator(arr)
+console.log(it.next().value)  // 1
+console.log(it.next().value)  // 2
+console.log(it.next().value)  // 3
+console.log(it.next().done)   // true
