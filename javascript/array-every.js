f6394f5159756837ffa07ba4ef27947ccab432ee
Bryan Andrade <bryand1@gmail.com>
2018-11-10 19:45:51 -0800

    Array functions

---
+++ javascript/array-every.js
@@ -0,0 +1,4 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const isBelowTen = (x) => x < 10;
+console.log(arr.every(isBelowTen));  // true
+
---
+++ javascript/array-some.js
@@ -0,0 +1,4 @@
+let arr = [1, 3, 5, 7, 8];
+const even = x => (x % 2) === 0;
+console.log(arr.some(even));  // true
+
