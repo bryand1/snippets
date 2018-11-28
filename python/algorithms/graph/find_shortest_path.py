db6a19b4e543f5cd7760613a80d8b00189a6bb98
Bryan Andrade <bryand1@gmail.com>
2018-11-27 19:07:58 -0800

    python.org graph algorithms

---
+++ python/algorithms/graph/find_shortest_path.py
@@ -0,0 +1,18 @@
+# https://www.python.org/doc/essays/graphs/
+
+
+def find_shortest_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    shortest = None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_shortest_path(graph, node, end, path)
+            if newpath:
+                if shortest is None or len(newpath) < len(shortest):
+                    shortest = newpath
+    return shortest
+
