0cbcb581f6dad1243e14255de34b6be1927ff70c
Bryan Andrade <bryand1@gmail.com>
2018-11-27 17:38:11 -0800

    c++ read input line into string variable

---
+++ c++/competitive-programming/read_line.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string s;
+  getline(cin, s);
+
+  cout << "Received a string." << endl;
+  cout << s << endl;
+
+  return 0;
+}
+
