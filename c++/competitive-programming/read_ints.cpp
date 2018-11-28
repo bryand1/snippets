bcb7a1e84ef86475a35ce527ac7bdf337c6bca45
Bryan Andrade <bryand1@gmail.com>
2018-11-27 17:42:58 -0800

    c++ loop through integer input

---
+++ c++/competitive-programming/read_ints.cpp
@@ -0,0 +1,13 @@
+// Loop through arbitrarily long integer input
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int x;
+  while (cin >> x) {
+    printf("%d\n", x);
+  }
+  return 0;
+}
+
