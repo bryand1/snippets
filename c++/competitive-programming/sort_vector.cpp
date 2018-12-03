e71ad4878c5294d40ce1a80dce0baa17b25483e6
Bryan Andrade <bryand1@gmail.com>
2018-12-02 23:04:37 -0800

    sort vector in reverse

--- c++/competitive-programming/sort_vector.cpp
+++ c++/competitive-programming/sort_vector.cpp
@@ -6,6 +6,8 @@ int main() {
     vector<int> v = {4, 2, 5, 3, 5, 8, 3};
     sort(v.begin(), v.end());
     cout << v[0] << endl;
+    sort(v.rbegin(), v.rend());
+    cout << v[0] << endl;
     return 0;
 }
 
