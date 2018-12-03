91783e98a847a4b7cce46c853faa5e0187361d08
Bryan Andrade <bryand1@gmail.com>
2018-12-02 23:03:17 -0800

    sort vector

---
+++ c++/competitive-programming/sort_vector.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> v = {4, 2, 5, 3, 5, 8, 3};
+    sort(v.begin(), v.end());
+    cout << v[0] << endl;
+    return 0;
+}
+
