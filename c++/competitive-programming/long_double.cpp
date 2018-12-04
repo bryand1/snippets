f5068f4d1421640155dc5699167f916a28a4f724
Bryan Andrade <bryand1@gmail.com>
2018-12-04 08:53:04 -0800

    long double typedef and cout.precision()

---
+++ c++/competitive-programming/long_double.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+typedef long double ld;
+const int N = 20;
+
+using namespace std;
+
+ld p[N][N];
+
+int main() { 
+    cout.precision(10);
+    cout << sizeof(ld) << endl;
+    p[0][0] = 0.6800001L;
+    cout << p[0][0] << endl;
+    return 0;
+}
