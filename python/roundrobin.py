3721b4c171d815bc2d0b662c2b10e87405b73949
Bryan Andrade <bryand1@gmail.com>
2019-03-01 10:35:27 -0800

    round robin - deque implementation

---
+++ python/roundrobin.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def roundrobin(*iterables):
+    """roundrobin('ABC', 'D', 'EF') --> A D E B F C"""
+    iterators = deque(map(iter, iterables))
+    while iterators:
+        try:
+            while True:
+                yield next(iterators[0])
+                iterators.rotate(-1)
+        except StopIteration:
+            iterators.popleft()
+
