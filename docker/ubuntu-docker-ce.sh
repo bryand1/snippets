55bbc15adaf259fb282b728702e88f6255a89563
Bryan Andrade <bryand1@gmail.com>
2019-11-07 10:16:37 -0800

    Install Docker on Ubuntu

--- docker/ubuntu-docker-ce.sh
+++ docker/ubuntu-docker-ce.sh
@@ -5,6 +5,7 @@ sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -13,4 +14,4 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io
