f779bf147c420996613b0778e243154cd750c3dd
Bryan Andrade <bryan.andrade@atlas.health>
2020-12-31 14:10:13 -0600

    Simplify Python 3.7.6 installation script

--- bash/install-python37.sh
+++ bash/install-python37.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
 # Install Python3.7 on Debian/Ubuntu
-# https://stackoverflow.com/questions/27022373/python3-importerror-no-module-named-ctypes-when-using-value-from-module-mul
 
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
 sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus
 sudo apt-get install libncursesw5-dev libgdbm-dev libc6-dev
 sudo apt-get install zlib1g-dev libsqlite3-dev tk-dev
 sudo apt-get install libssl-dev openssl
 sudo apt-get install libffi-dev
-
-# Added May 17, 2019
 sudo apt-get install libbz2-dev
 sudo apt-get install libreadline-dev
 
